4bd390eb73217d3a163a770230d3a516f92e6bc3
vineetsemwal <vineetsemwal82@gmail.com>
2021-12-21 13:02:53 +0530

    task 1 added(pages + panels)

--- day3/panel/myproject/src/main/java/com/mycompany/SellerDetailsPanel.java
+++ day3/panel/myproject/src/main/java/com/mycompany/SellerDetailsPanel.java
@@ -3,19 +3,30 @@ package com.mycompany;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
 
 public class SellerDetailsPanel extends Panel {
+
+
+    private int counter; // incrementing
 
     public SellerDetailsPanel(String id, IModel model){
         super(id, model);
     }
 
+    public IModel<Seller>getSellerModel(){
+        return (IModel<Seller>)getDefaultModel();
+    }
+
+    public Seller getSeller(){
+        return getSellerModel().getObject();
+    }
+
     @Override
     protected void onInitialize() {
         super.onInitialize();
-        Seller seller=(Seller)getDefaultModelObject();
-        add(new Label("name",()->seller.getName()));
-        add(new Label("sellerid",()->seller.getId()));
+        add(new Label("name",()->getSeller().getName()));
+        add(new Label("sellerid",()->getSeller().getId()));
 
     }
 }
