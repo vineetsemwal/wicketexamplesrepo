b5d86d3e08625e57199ed5f20e355a1ba64aa396
vineetsemwal <vineetsemwal82@gmail.com>
2021-12-21 20:54:14 +0530

    proof add compoents or work in constructor is not good

---
+++ day4/proof_workinconstructorisbad/myproject/src/main/java/com/mycompany/HomePage.java
@@ -0,0 +1,27 @@
+package com.mycompany;
+
+import org.apache.wicket.Page;
+import org.apache.wicket.RestartResponseException;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.*;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.WebPage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class HomePage extends WebPage {
+    private static final long serialVersionUID = 1L;
+
+    private static final Logger log= LoggerFactory.getLogger(HomePage.class);
+
+    public HomePage(final PageParameters parameters) {
+        super(parameters);
+        add(new SamplePanel("panel"));
+
+    }
+
+
+}
